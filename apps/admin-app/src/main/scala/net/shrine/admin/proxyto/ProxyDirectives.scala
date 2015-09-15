ad2a38aebdee8e55447ec536c73db61b7e6960a0
David Walend <david_walend@hms.harvard.edu>
2015-09-15 10:58:41 -0400

    Fixed 'Host' header value doesn't match request target authority problem by stripping out the old header.
    Former-commit-id: 9f49e0512184857bf5816cc5c88c363f2a0cdd03
    Former-commit-id: 8cdf6bcea5fd9244751daa3b503d45a0878ea757

--- apps/admin-app/src/main/scala/net/shrine/admin/proxyto/ProxyDirectives.scala
+++ apps/admin-app/src/main/scala/net/shrine/admin/proxyto/ProxyDirectives.scala
@@ -3,12 +3,12 @@ package net.shrine.admin.proxyto
 import spray.can.Http
 import akka.io.IO
 import akka.actor.ActorSystem
-import spray.http.{ HttpRequest, Uri }
+import spray.http.{HttpHeader, HttpHeaders, HttpRequest, Uri}
 import spray.routing.{Route, RequestContext}
 
 /**
- * From https://github.com/bthuillier/spray/commit/d31fc1b5e1415e1b908fe7d1f01f364a727e2593 . Replace when Spray has
- * its own proxy if this class is even still in use.
+ * From https://github.com/bthuillier/spray/commit/d31fc1b5e1415e1b908fe7d1f01f364a727e2593 with extra bits from http://www.cakesolutions.net/teamblogs/http-proxy-with-spray.
+ * Replace when Spray has its own proxy if this class is even still in use then.
  *
  * @author david
  * @since 9/14/15
@@ -20,12 +20,20 @@ trait ProxyDirectives {
     ctx ⇒ transport.tell(f(ctx), ctx.responder)
   }
 
+  private def stripHostHeader(headers: List[HttpHeader] = Nil) =
+    headers filterNot (header => header is (HttpHeaders.Host.lowercaseName))
+
   /**
    * proxy the request to the specified uri
    *
    */
   def proxyTo(uri: Uri)(implicit system: ActorSystem): Route = {
-    sending(_.request.copy(uri = uri))
+    sending(ctx =>
+      ctx.request.copy(
+        uri = uri,
+        headers = stripHostHeader(ctx.request.headers)
+      )
+    )
   }
 
   /**
@@ -33,7 +41,12 @@ trait ProxyDirectives {
    *
    */
   def proxyToUnmatchedPath(uri: Uri)(implicit system: ActorSystem): Route = {
-    sending(ctx ⇒ ctx.request.copy(uri = uri.withPath(uri.path.++(ctx.unmatchedPath))))
+    sending(ctx ⇒
+      ctx.request.copy(
+        uri = uri.withPath(uri.path.++(ctx.unmatchedPath)),
+        headers = stripHostHeader(ctx.request.headers)
+      )
+    )
   }
 }
 
