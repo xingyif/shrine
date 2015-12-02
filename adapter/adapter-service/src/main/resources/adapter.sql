ad33ac3b013e4ad998e2f3247a49ff630e5950a7
David Walend <david_walend@hms.harvard.edu>
2015-12-02 15:36:08 -0500

    Added default values to new columns
    Former-commit-id: 02ffa58dfb4aaff269864f62a85b2e2a6ce7452e
    Former-commit-id: 82e62c3e2bfa72f4b95547e20dfd3b5d7b5598af

--- adapter/adapter-service/src/main/resources/adapter.sql
+++ adapter/adapter-service/src/main/resources/adapter.sql
@@ -39,9 +39,9 @@ create table ERROR_RESULT(
 
 alter table ERROR_RESULT add column CODEC varchar(256) not null default "Pre-1.20 Error"
 alter table ERROR_RESULT add column STAMP varchar(256) not null default "Unknown time and machine"
-alter table ERROR_RESULT add column SUMMARY text not null
-alter table ERROR_RESULT add column PROBLEM_DESCRIPTION text not null
-alter table ERROR_RESULT add column DETAILS text not null
+alter table ERROR_RESULT add column SUMMARY text not null default "Pre-1.20 Error"
+alter table ERROR_RESULT add column PROBLEM_DESCRIPTION text not null default "Pre-1.20 Error"
+alter table ERROR_RESULT add column DETAILS text not null default "Pre-1.20 Error"
 
 create table COUNT_RESULT(
   id int not null auto_increment,
