7db618b0b59dd83932fe68995008d1944b156c05
David Walend <david_walend@hms.harvard.edu>
2015-12-03 15:53:33 -0500

    Try getting status service to just respond to calls from the same machine.
    Former-commit-id: 887c29b4a946d81d60d2bd77a9fc8e9dfba012ce
    Former-commit-id: 472b23c0b5126892803018844f7fa243a6d43014

--- apps/shrine-app/src/main/scala/net/shrine/status/StatusJaxrs.scala
+++ apps/shrine-app/src/main/scala/net/shrine/status/StatusJaxrs.scala
@@ -1,10 +1,9 @@
 package net.shrine.status
 
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
+import javax.ws.rs.{WebApplicationException, GET, Path, Produces}
+import javax.ws.rs.core.{Response, MediaType}
 
+import com.sun.jersey.spi.container.{ContainerRequest, ContainerRequestFilter}
 import com.typesafe.config.{Config => TsConfig, ConfigValue}
 import org.json4s.{DefaultFormats, Formats}
 import org.json4s.native.Serialization
@@ -14,6 +13,8 @@ import net.shrine.log.Loggable
 import scala.collection.JavaConverters._
 import scala.collection.immutable.{Map,Set}
 
+import net.shrine.config.ConfigExtensions
+
 /**
   * A subservice that shares internal state of the shrine servlet.
   *
@@ -22,7 +23,7 @@ import scala.collection.immutable.{Map,Set}
   */
 @Path("/status")
 @Produces(Array(MediaType.APPLICATION_JSON))
-case class StatusJaxrs(shrineConfig:TsConfig) extends Loggable {
+case class StatusJaxrs(shrineConfig:TsConfig) extends Loggable with ContainerRequestFilter {
 
   implicit def json4sFormats: Formats = DefaultFormats
 
@@ -39,6 +40,16 @@ case class StatusJaxrs(shrineConfig:TsConfig) extends Loggable {
   def config: String = {
     //todo probably better to reach out and grab the config from ManuallyWiredShrineJaxrsResources once it is a singleton
     Serialization.write(Json4sConfig(shrineConfig))
+  }
+
+  override def filter(requestContext: ContainerRequest): ContainerRequest = {
+    val hostOfOrigin = requestContext.getBaseUri.getHost
+    val permittedHostOfOrigin:String = shrineConfig.getOption("shrine.status.permittedHostOfOrigin",_.getString).getOrElse(java.net.InetAddress.getLocalHost.getHostName)
+    if(hostOfOrigin == permittedHostOfOrigin) requestContext
+    else {
+      val response = Response.status(Response.Status.UNAUTHORIZED).entity(s"Only available from $permittedHostOfOrigin").build()
+      throw new WebApplicationException(response)
+    }
   }
 }
 
