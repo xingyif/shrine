e9754165bdce84f5a2f59d4642b6036d7df45d0e
Ben Carmen <benjamin_carmen@hms.harvard.edu>
2016-01-07 11:36:26 -0500

    Commit of POSO stuff for Status
    Former-commit-id: 71c32e0c1e665309f28b1c25bf10dc71efc15da8
    Former-commit-id: cb00b38d34727c8f26df1a9d8b292e104497bc1e

---
+++ commons/protocol/src/main/scala/net/shrine/status/protocol/Config.scala
@@ -0,0 +1,32 @@
+package net.shrine.status.protocol
+
+import com.typesafe.config.{Config => TsConfig, ConfigRenderOptions}
+import net.liftweb.json.Serialization
+
+
+import scala.collection.immutable.{Set, Map}
+
+/**
+ * This class is for representing a shrine.conf file, note that password data has been
+ * redacted.
+ *
+ * Created by ben on 12/15/15.
+ */
+//todo SortedMap
+case class Config(keyValues:Map[String,String]){
+
+}
+
+object Config{
+  def isPassword(key:String):Boolean = {
+    key.toLowerCase.contains("password")
+  }
+
+  def apply(config:TsConfig):Config = {
+    import scala.collection.JavaConverters._
+    val entries: Set[(String, String)] = config.entrySet.asScala.to[Set].map(x => (x
+      .getKey,x.getValue.render())).filterNot(x => isPassword(x._1))
+    val sortedMap: Map[String, String] = entries.toMap
+    Config(sortedMap)
+  }
+}
---
+++ commons/protocol/src/main/scala/net/shrine/status/protocol/Version.scala
@@ -0,0 +1,6 @@
+package net.shrine.status.protocol
+
+/**
+ * Created by ben on 12/15/15.
+ */
+case class Version(version:String)
