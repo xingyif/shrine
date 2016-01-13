0751b1f6f5cb8d8ce19a99b14dae5217471d7c76
David Walend <david_walend@hms.harvard.edu>
2016-01-13 15:56:24 -0500

    Comment for java code's reason to exist
    Former-commit-id: 852f9f302d7ee0ae1f5bd470ff6c414df2cb8aa6
    Former-commit-id: de1cc3bf728d1226da007d576bb3539e2e1ecd10

--- apps/dashboard-app/src/main/java/net/shrine/dashboard/jwtauth/SigningKeyResolverBridge.java
+++ apps/dashboard-app/src/main/java/net/shrine/dashboard/jwtauth/SigningKeyResolverBridge.java
@@ -6,6 +6,12 @@ import io.jsonwebtoken.SigningKeyResolverAdapter;
 
 import java.security.Key;
 
+/**
+ * SigningKeyResolverBridge is in Java because JwsHeader takes a parameter, but SigningKeyResolverAdapter specifies it as a raw type.
+ *
+ * @author david
+ * @since 1.21
+ */
 public class SigningKeyResolverBridge extends SigningKeyResolverAdapter {
 
     public Key resolveSigningKey(JwsHeader header, Claims claims) {
