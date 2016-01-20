a5370351263929c1abe452e337b8104d768cb871
David Walend <david_walend@hms.harvard.edu>
2016-01-20 16:26:14 -0500

    cleaned up a lot of dead sql code. SHRINE-1263
    Former-commit-id: 87fdce25c99e17a5222d6ab5eef623810cd16f5e
    Former-commit-id: a973ecc414c615ea3037d32f2cb85adac0ce5262

--- adapter/adapter-service/src/main/resources/adapter-drop.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists PRIVILEGED_USER;
-drop table if exists ERROR_RESULT;
-drop table if exists COUNT_RESULT;
-drop table if exists BREAKDOWN_RESULT;
-drop table if exists PATIENT_SET;
-drop table if exists QUERY_RESULT;
-drop table if exists SHRINE_QUERY;
--- adapter/adapter-service/src/main/resources/adapter-migrate-schema-to-1.18.sql
+++
@@ -1,3 +0,0 @@
-alter table SHRINE_QUERY modify query_expression text null;
-alter table SHRINE_QUERY add query_xml text null;
-
