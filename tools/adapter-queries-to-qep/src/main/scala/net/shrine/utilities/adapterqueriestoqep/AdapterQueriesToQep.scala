4983b6dfac2bad9aa6c02ff79ce737df5657f546
David Walend <david_walend@hms.harvard.edu>
2016-03-01 15:59:16 -0500

    Now loading the config
    Former-commit-id: 39f39634af1deaa734f349bf91cd75263dad9f38
    Former-commit-id: 8d0dd87ee32687ae4157563d9945e8ba8c923fd1

--- tools/adapter-queries-to-qep/src/main/scala/net/shrine/utilities/adapterqueriestoqep/AdapterQueriesToQep.scala
+++ tools/adapter-queries-to-qep/src/main/scala/net/shrine/utilities/adapterqueriestoqep/AdapterQueriesToQep.scala
@@ -1,5 +1,9 @@
 package net.shrine.utilities.adapterqueriestoqep
 
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
+
 /**
  * @author dwalend
  * @since 1.21
@@ -7,6 +11,15 @@ package net.shrine.utilities.adapterqueriestoqep
 
 object AdapterQueriesToQep {
   def main(args: Array[String]): Unit = {
-    println("hello")
+    if(args.length < 2) throw new IllegalArgumentException("Requires at least two arguments, full paths to the adapter-queries-to-qep.conf file and the shrine.conf file.")
+
+    val localConfig = args(0)
+    val shrineConfig = args(1)
+
+    val config = ConfigFactory.parseFile(new File(localConfig)).withFallback(ConfigFactory.load(shrineConfig))
+
+
+
+
   }
 }
