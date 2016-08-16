2f942c05274d393295553645619f0cd4c692b6d8
Ty <Ty_Coghlan@hms.harvard.edu>
2016-08-16 12:00:26 -0400

    Removed .idea-ignore files
    Former-commit-id: 0f3e602bd2b64e4a79f67b35a4a831259437e69e
    Former-commit-id: d3370cd7bda2d4d37fe15b6408ad0895f26362ae

--- commons/util/src/main/scala/net/shrine/problem/DashboardProblemsDatabase.scala
+++ commons/util/src/main/scala/net/shrine/problem/DashboardProblemsDatabase.scala
@@ -57,7 +57,7 @@ object Problems {
     def stampText = column[String]("stampText")
     def summary = column[String]("summary")
     def description = column[String]("description")
-    def xml = column[String]("detailsXml")
+    def xml = column[String]("detailsXml", O.SqlType("Clob"))
     def epoch= column[Long]("epoch")
     // projection between table row and problem digest
     def * = (id, codec, stampText, summary, description, xml, epoch) <> (rowToProblem, problemToRow)
