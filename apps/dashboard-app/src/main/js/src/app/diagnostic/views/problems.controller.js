161b637b468a68279a86dad664df1713bf144f74
Ty <Ty_Coghlan@hms.harvard.edu>
2016-09-19 16:32:45 -0400

    WIP on getting reactive button
    Former-commit-id: ad52afb8f409bb32490ea0bd1204511be579edb5
    Former-commit-id: 6a1557290ec600de9b883de8b6e8452f62797469

--- apps/dashboard-app/src/main/js/src/app/diagnostic/views/problems.controller.js
+++ apps/dashboard-app/src/main/js/src/app/diagnostic/views/problems.controller.js
@@ -59,8 +59,8 @@
         }
     });
 
-    ProblemsController.$inject = ['$app', '$log', '$sce'];
-    function ProblemsController ($app, $log, $sce) {
+    ProblemsController.$inject = ['$app', '$log', '$sce', '$scope'];
+    function ProblemsController ($app, $log, $sce, $scope) {
         var vm = this;
 
         init();
@@ -69,6 +69,7 @@
          *
          */
         function init () {
+            vm.$log = $log;
             vm.date = new Date();
             vm.isOpen = { 'open': false };
             vm.pageSizes = [5, 10, 20];
@@ -77,13 +78,15 @@
             vm.newPage = newPage;
             vm.floor = Math.floor;
             vm.handleButton = handleButton;
-            vm.open = function() { vm.isOpen.open = !vm.isOpen.open;};
-            vm.pageSizeCheck = function(n) { return n < vm.probsSize };
-            vm.parseDetails = function(details) { return $sce.trustAsHtml(parseDetails(details)) };
-            vm.stringify = function(arg) { return JSON.stringify(arg, null, 2); };
-            vm.numCheck = function(any) { return isFinite(any)? (any - 1) * vm.probsN: vm.probsOffset };
-            vm.changePage = function() { vm.newPage(vm.probsOffset, vm.pageSize > 20? 20: vm.pageSize < 0? 0: vm.pageSize) };
+            vm.checkDate =     function(date)    {$log.warn(date); return date != undefined };
+            vm.open =          function()        { vm.isOpen.open = !vm.isOpen.open;};
+            vm.pageSizeCheck = function(n)       { return n < vm.probsSize };
+            vm.parseDetails =  function(details) { return $sce.trustAsHtml(parseDetails(details)) };
+            vm.stringify =     function(arg)     { return JSON.stringify(arg, null, 2); };
+            vm.numCheck =      function(any)     { return isFinite(any)? (any - 1) * vm.probsN: vm.probsOffset };
+            vm.changePage =    function()        { vm.newPage(vm.probsOffset, vm.pageSize > 20? 20: vm.pageSize < 0? 0: vm.pageSize) };
 
+            $scope.$watch('date', function() {$scope.$apply()});
 
             //todo: Get rid of this and figure out something less hacky
             vm.formatCodec = function(word) {
