5fd6269a36f38f6e1018c16e4e06b812b2c6c924
Ty <Ty_Coghlan@hms.harvard.edu>
2016-11-01 16:29:47 -0400

    wrote out simple hubCert test
    Former-commit-id: b32cde68934a0472c57e5f027721cd1b1b7c1099
    Former-commit-id: 55e8cb52470887159ad7a843c69dfb1caeb44cb8

--- commons/crypto/src/test/scala/net/shrine/crypto2/HubCertCollectionTest.scala
+++ commons/crypto/src/test/scala/net/shrine/crypto2/HubCertCollectionTest.scala
@@ -1,5 +1,6 @@
 package net.shrine.crypto2
 
+import junit.framework.TestFailure
 import net.shrine.crypto.{KeyStoreDescriptor, KeyStoreType, TestKeystore}
 import net.shrine.util.SingleHubModel
 import org.junit.runner.RunWith
@@ -12,8 +13,20 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class HubCertCollectionTest extends FlatSpec with Matchers {
   val descriptor = KeyStoreDescriptor("crypto2/shrine-test.jks", "justatestpassword", None, Nil, KeyStoreType.JKS, SingleHubModel)
-  val hubCertCollection = BouncyKeyStoreCollection.fromFileRecoverWithClassPath(descriptor)
+  val heyo = "Heyo!".getBytes("UTF-8")
 
-  hubCertCollection.allEntries.size shouldBe 2
 
+  "A hub cert collection" should "build and verify its own messages" in {
+    val hubCertCollection = BouncyKeyStoreCollection.fromFileRecoverWithClassPath(descriptor) match {
+      case hub:HubCertCollection => hub
+      case _ => fail("This should generate a HubCertCollection!")
+    }
+
+    hubCertCollection.allEntries.size shouldBe 2
+    hubCertCollection.verifyBytes(hubCertCollection.signBytes(heyo), heyo)
+    hubCertCollection.myEntry.privateKey.isDefined shouldBe true
+    hubCertCollection.caEntry.privateKey.isDefined shouldBe false
+    hubCertCollection.myEntry.aliases.first shouldBe "shrine-test"
+    hubCertCollection.caEntry.aliases.first shouldBe "shrine-test-ca"
+  }
 }
