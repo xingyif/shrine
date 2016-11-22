f1bee95fd15339f5bbb7255625e0a28fab02426e
Ty <Ty_Coghlan@hms.harvard.edu>
2016-11-22 11:02:24 -0500

    better error reporting for InavlidVerifySignatureResponse, default info message for metaData
    Former-commit-id: cdfb1e4adc90db1e0fc0d09a53dff67d55a3923b
    Former-commit-id: 7d4334791f40ee0d36ad208fc527b696c262ff14

--- apps/meta-app/src/main/scala/net/shrine/metadata/MetaDataService.scala
+++ apps/meta-app/src/main/scala/net/shrine/metadata/MetaDataService.scala
@@ -13,6 +13,17 @@ import scala.util.Try
   */
 trait MetaDataService extends HttpService with Loggable {
   lazy val config = ConfigSource.config.getConfig("shrine.metaData")
+  val info =
+    """
+      |The SHRINE Metadata service. This is a simple API that gives you
+      |read access to the metaData section within SHRINE's configuration.
+      |You can access this data by key, or by accessing the entire metaData
+      |config section at once. To access everything at once, make a GET
+      |to shrine-metadata/data (if on a browser, just add /data to the
+      |end of the current url). To access values by key, make a GET to
+      |shrine-metadata/data?key={{your key here without braces}} (again,
+      |if on a browser just add /data?key={{your key}} to the end of the url).
+    """.stripMargin
 
   lazy val route: Route = get {
     pathPrefix("ping") { complete("pong")} ~
@@ -20,7 +31,7 @@ trait MetaDataService extends HttpService with Loggable {
       parameter("key") { (key: String) =>
         complete(handleKey(key))
       } ~ complete(handleAll)
-    }}
+    }} ~ complete(info)
 
   def handleAll:(StatusCode, String) = {
     StatusCodes.OK -> config.root.render(ConfigRenderOptions.concise())
