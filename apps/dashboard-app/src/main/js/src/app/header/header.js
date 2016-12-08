4d4e868979be289caa17740b518a037f265714b4
Ty <Ty_Coghlan@hms.harvard.edu>
2016-12-08 13:20:14 -0500

    reverting header
    Former-commit-id: 4af679bf1b7d1109a4fb1ff5f3ee0de545e855dd
    Former-commit-id: 1ee1d07aadc11d017f8a79c04fd33b310f9785c4

--- apps/dashboard-app/src/main/js/src/app/header/header.js
+++ apps/dashboard-app/src/main/js/src/app/header/header.js
@@ -1,57 +1,68 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    // -- register directive with angular -- //
-    angular.module('shrine-tools')
-        .directive('header', Header);
 
-    /**
-     *
-     * @returns {{restrict: string, replace: boolean, templateUrl: string,
+	// -- register directive with angular -- //
+	angular.module('shrine-tools')
+		.directive('header', Header);
+
+
+	/**
+	 *
+	 * @returns {{restrict: string, replace: boolean, templateUrl: string,
 	 * controller: HeaderController, controllerAs: string, link: HeaderLinker, scope: {title: string}}}
-     * @constructor
-     */
-    function Header() {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'src/app/header/header.tpl.html',
-            controller: HeaderController,
-            controllerAs: 'vm',
-            link: HeaderLinker,
-            scope: {
-                title: '@'
-            }
-        }
-    }
+	 * @constructor
+	 */
+	function Header () {
+		return {
+			restrict: 		'E',
+			replace: 		true,
+			templateUrl: 	'src/app/header/header.tpl.html',
+			controller: 	HeaderController,
+			controllerAs: 	'vm',
+			link:			HeaderLinker,
+			scope: {
+				title: '@'
+			}
+		}
+	}
 
-    /**
-     *
-     * @type {string[]}
-     */
-    HeaderController.$inject = ['$app', '$scope', '$location'];
-    function HeaderController($app, $scope, $location) {
-        $scope.m = $app.model.m;
-        $scope.goHome = goHome;
+
+	/**
+	 *
+	 * @type {string[]}
+	 */
+	HeaderController.$inject = ['$app', '$scope', '$location'];
+	function HeaderController($app, $scope, $location) {
+		$scope.m = $app.model.m;
+		$scope.goHome = goHome;
+
 
         function goHome() {
-            //todo remove duplication with dashboardcontroller
+
             $app.model.toDashboard.url = '';
             $app.model.m.siteAlias = '';
-            $app.model.clearCache();
-            $location.url("/diagnostic/summary");
-            $location.url("/diagnostic/problems");
+            clearCache();
             $location.url("/diagnostic/summary");
         }
-    }
 
-    /**
-     *
-     * @type {string[]}
-     */
-    HeaderLinker.$inject = ['scope', 'element', 'attributes'];
-    function HeaderLinker(s, e, a) {
-        var vm = s.vm;
-    }
+        function clearCache() {
+            for (var member in $app.model.cache) {
+                if($app.model.cache.hasOwnProperty(member)) delete $app.model.cache[member];
+            }
+        }
+	}
+
+	/**
+	 *
+	 * @type {string[]}
+	 */
+	HeaderLinker.$inject = ['scope', 'element', 'attributes' ];
+	function HeaderLinker (s, e, a) {
+		var vm = s.vm;
+	}
 
 })();
+
+
+
