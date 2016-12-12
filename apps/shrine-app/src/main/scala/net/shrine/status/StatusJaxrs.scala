a9eca6dae0a1c39762e6fbf047fbc3203796ea3e
Ty <Ty_Coghlan@hms.harvard.edu>
2016-12-12 15:25:58 -0500

    minor commenting/formatting
    Former-commit-id: eb8b6654652919243cc8461c2028900be4cdc51c
    Former-commit-id: bb6bee1730d83823ac6e2b6abc1d0cbb60ab8fdf

--- apps/shrine-app/src/main/scala/net/shrine/status/StatusJaxrs.scala
+++ apps/shrine-app/src/main/scala/net/shrine/status/StatusJaxrs.scala
@@ -185,7 +185,6 @@ object KeyStoreReport {
       expires = certCollection.myEntry.cert.getNotAfter.getTime,
       md5Signature = UtilHasher.encodeCert(certCollection.myEntry.cert, "MD5"),
       sha256Signature = UtilHasher.encodeCert(certCollection.myEntry.cert, "SHA-256"),
-      //todo sha1 signature if needed
       caTrustedAlias = maybeCaEntry.map(_.aliases.first),
       caTrustedSignature = maybeCaEntry.map(entry => UtilHasher.encodeCert(entry.cert, "MD5")),
       remoteSiteStatuses = blockForSiteStatuses,
@@ -249,6 +248,7 @@ object Qep {
   def apply(): Qep = new Qep(
     maxQueryWaitTimeMillis = queryEntryPointComponents.fold(0L)(_.i2b2Service.queryTimeout.toMicros),
     create = queryEntryPointComponents.isDefined,
+    //todo: delete attatchSingingCert
     attachSigningCert = queryEntryPointComponents.fold(false)(_.i2b2Service.broadcastAndAggregationService.attachSigningCert),
     authorizationType = queryEntryPointComponents.fold("")(_.i2b2Service.authorizationService.getClass.getSimpleName),
     includeAggregateResults = queryEntryPointComponents.fold(false)(_.i2b2Service.includeAggregateResult),
