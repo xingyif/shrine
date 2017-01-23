44048b3cf2bbffa67836c3e8742b82662b182c7e
ben-carmen <benjamin_carmen@hms.harvard.edu>
2017-01-23 14:59:48 -0500

    working on node pm server
    Former-commit-id: ce38a9f60193d67ed28c22a92fce3c5b9559ab2c
    Former-commit-id: 94d2d13c8679c972f249edaa1e340089029fdea1

--- shrine-webclient/src/main/html/mock-server/server.js
+++ shrine-webclient/src/main/html/mock-server/server.js
@@ -1,14 +1,15 @@
 
 //http://localhost:8000/shrine-proxy/request
-var express = require('express');       
+var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
-var app = express();                
+var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 6443;
 var router = express.Router();
 var fs = require('fs');
+var isAuthorized = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,7 +21,7 @@ app.use(bodyParser.xml({
     explicitArray: false // Only put nodes in array if >1 
   }
 }));
- 
+
 app.use(cors());
 app.use('/shrine-proxy/request', router);
 app.listen(port);
@@ -29,16 +30,58 @@ console.log('I2B2SERVER Mock Server started on port: ' + port);
 // -- routes --//
 
 router.post('/', function (req, res) {
-  // Any request with an XML payload will be parsed 
-  // and a JavaScript object produced on req.body 
-  // corresponding to the request payload. 
-  
- //console.log(req.body);
- var xml = fs.readFileSync('./getUserAuth.xml');
+  var requestType = parseRequestType(req);
+  var fileName = getFilename(requestType);
 
- 
-  res.header('Content-Type','text/xml').send(xml);
-  
+  console.log('\n\n');
+  console.log(req.body);
+   
 
-  //res.send('<xml>i2b2 success</xml>');
+  var xml = fs.readFileSync('./getUserAuth.xml');
+  res.header('Content-Type', 'text/xml').send(xml);
 });
+
+
+
+function getFilename (requestType) {
+  switch (requestType) {
+
+    case 'i2b2:request':
+      return 'getUserAuth.xml';
+      break;
+
+    case 'CRC_QRY_getResultType':
+      return 'getQRY_getResultType.xml'
+      break;
+    
+    case 'CRC_QRY_getQueryMasterList_fromUserId':
+      return 'getQueryMasterList_fromUserId.xml'
+      break;
+  
+    default:
+      return null;
+      break;
+  }
+}
+
+
+/*
+ Any request with an XML payload will be parsed and a JavaScript object produced on req.body 
+ corresponding to the request payload. 
+ This is messy refactor.
+*/
+function parseRequestType(req) {
+
+  if(req.body['i2b2:request']){
+    return 'i2b2:request';
+  }
+
+  if (req.body['ns6:request']) {
+    return req.body['ns6:request'].message_body['ns4:psmheader'].request_type;
+  }
+
+  if (req.body['ns6:request']) {
+    return req.body['ns6:request'].message_body['ns4:psmheader'].request_type;
+  }
+
+}
