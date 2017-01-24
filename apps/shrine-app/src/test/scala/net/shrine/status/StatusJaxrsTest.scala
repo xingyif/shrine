c0f20f69c32e7b2ce78951cfe2b4d8aa4cc68cd3
Ty <Ty_Coghlan@hms.harvard.edu>
2017-01-24 11:24:36 -0500

    merging
    Former-commit-id: 5a1a6c69e4830f08cece33995d5573c3e0055748
    Former-commit-id: 8d49b773625b59066e9129359547d2882180c4c9

--- apps/shrine-app/src/test/scala/net/shrine/status/StatusJaxrsTest.scala
+++ apps/shrine-app/src/test/scala/net/shrine/status/StatusJaxrsTest.scala
@@ -17,7 +17,7 @@ import scala.collection.immutable.Map
 class StatusJaxrsTest extends ShouldMatchersForJUnit {
 
   implicit def json4sFormats: Formats = DefaultFormats
-  val expectedConfig = ConfigFactory.load("shrine") //new File("/Users/ty/shrine/apps/shrine-app/src/test/resources/shrine.conf"))
+  val expectedConfig = ConfigFactory.load("shrine")
   val statusJaxrs = StatusJaxrs(expectedConfig)
 
   @Test
@@ -34,7 +34,6 @@ class StatusJaxrsTest extends ShouldMatchersForJUnit {
 
     val configString = statusJaxrs.config
     val config = Serialization.read[Json4sConfig](configString)
-
     config should equal(expectedJson4sConfig)
 
     val passwordKeys = config.keyValues.filter(x => Json4sConfig.isPassword(x._1))
