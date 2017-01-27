d6162efe4e0531db4f3ffe771598b93b68e1204a
Ben Carmen <ben@Bens-MacBook-Pro.local>
2017-01-26 19:36:01 -0500

    changed the Webclient build directory in maven from html to js, also cleaned up some unneeded files.
    Former-commit-id: 651f7f41b3dd10f0a6d6eed6bba45e26d224d502
    Former-commit-id: a650389cb69e04af2086b1a00bb2365fa4465966

---
+++ shrine-webclient/src/main/js/js-i2b2/cells/SHRINE/cell_config_data.js
@@ -0,0 +1,19 @@
+// this file contains a list of all files that need to be loaded dynamically for this i2b2 Cell
+// every file in this list will be loaded after the cell's Init function is called
+{
+  files: [
+    "shrine.controller.js",
+    "i2b2_msgs.js",
+    "shrine.plugin.js"
+  ],
+  css: [],
+  config: {
+    // additional configuration variables that are set by the system
+    name: "SHRINE Cell",
+    description: "The SHRINE cell...",
+    category: ["core","cell","shrine"],
+    newTopicURL: "https://shrine-hostname.here:6443/steward/client/index.html",
+    readApprovedURL:"https://shrine-hostname.here:6443/shrine/rest/i2b2/request",
+    wrapperHtmlFile: "./js-shrine/shrine.plugin.html"
+  }
+}
