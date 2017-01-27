a6b482265b30e0dd00976a687f458a04280a9121
Ben Carmen <ben@Bens-MacBook-Pro.local>
2017-01-26 20:04:00 -0500

    webclient directory cleanup and maven pom target updates
    Former-commit-id: 71ebb85e34f9ff4f62d61e321838c15dffc5dbd4
    Former-commit-id: c3eb6a05579b5545304b5a4bff4226ab65776390

---
+++ shrine-webclient/src/main/js/client/js-shrine/pm-mock/server.js
@@ -0,0 +1,81 @@
+
+//http://localhost:8000/shrine-proxy/request
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 6443;
+var router = express.Router();
+var fs = require('fs');
+var isAuthorized = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.xml({
+  limit: '1GB',   // Reject payload bigger than 1 MB 
+  xmlParseOptions: {
+    normalize: true,     // Trim whitespace inside text nodes 
+    normalizeTags: true, // Transform tags to lowercase 
+    explicitArray: false // Only put nodes in array if >1 
+  }
+}));
+
+app.use(cors());
+app.use('/shrine-proxy/request', router);
+app.listen(port);
+console.log('I2B2SERVER Mock Server started on port: ' + port);
+
+// -- routes --//
+
+router.post('/', function (req, res) {
+  var requestType = parseRequest(req);
+  var fileName = getFilename(requestType);
+  var xml = fs.readFileSync('./' + fileName);
+  
+  res.header('Content-Type', 'text/xml').send(xml);
+});
+
+
+
+
+
+/*
+ Any request with an XML payload will be parsed and a JavaScript requestect produced on req.body 
+ corresponding to the request payload. 
+ This is messy refactor.
+*/
+function parseRequest(request) {
+  request = request.body;
+  var requestTypes = ['i2b2:request', 'ns2:request', 'ns3:request', 'ns6:request'];
+  var requestType = requestTypes.find(t => request[t] !== undefined);
+  request =  (request[requestType] && request[requestType].message_body) ?
+    request[requestType].message_body : null;
+
+    return parseBodyRequest(request);
+}
+
+function parseBodyRequest(request) {
+  var bodyTypes = ['pm:get_user_configuration', 'ns4:get_categories', 'ns4:get_schemes', 'ns7:sheriff_header', 'ns4:psmheader'];
+  var bodyType = bodyTypes.find(t => request[t] !== undefined);
+
+  if(bodyType == 'ns4:psmheader') {
+    bodyType = request['ns4:psmheader'].request_type;
+  }
+
+  return bodyType;
+}
+
+function getFilename(value) {
+  let fileMap = {
+    'pm:get_user_configuration': 'getUserAuth.xml',
+    'ns4:get_categories': 'GetCategories.xml',
+    'ns4:get_schemes': 'GetSchemes.xml',
+    'ns7:sheriff_header': 'getUserAuth.xml',
+    'CRC_QRY_getResultType': 'getQRY_getResultType.xml',
+    'CRC_QRY_getQueryMasterList_fromUserId': 'getQueryMasterList_fromUserId.xml'
+  };
+
+  return fileMap[value];
+}
