1782a80ed2acb189a9a19f8c13cf6d97fe167dc5
Ty <Ty_Coghlan@hms.harvard.edu>
2017-01-31 16:43:06 -0500

    further work on query json structures
    Former-commit-id: c453486b555616b12e6918b0524cbdd476d97b1f
    Former-commit-id: 47aa452e3aa8d9b808edb1e47c9502fd71ce4b7f

--- qep/service/src/main/scala/net/shrine/qep/queries/QepQueryJson.scala
+++ qep/service/src/main/scala/net/shrine/qep/queries/QepQueryJson.scala
@@ -12,9 +12,16 @@ import scala.util.hashing.Hashing.default
 /**
   * @by ty
   * @since 1/31/17
+  * This uses a class, object, and trait to define one entity.
+  * Should I just get rid of the trait altogether?
+  * Also, everything in the class and companion object will be the same,
+  * except for the fields that it's working with. Would be a good place
+  * to introduce a macro to reduce boilerplate, possibly
   */
 
 final class QueryJson(val json: Json) extends QueryJsonTrait {
+  // Structural equality on everything except the underlying json.
+  // Can always get full equality by this.json == that.json
   override def equals(that: scala.Any): Boolean = that match {
     case that:QueryJson => QueryJson.unapply(this) == QueryJson.unapply(that)
     case _ => false
@@ -24,6 +31,10 @@ final class QueryJson(val json: Json) extends QueryJsonTrait {
 }
 
 object QueryJson {
+  def apply(json:Json): Option[QueryJson] =
+    if (json.is[QueryJson]) Some(json.as[QueryJson]) else None
+
+  // Allows easier pattern matching on all the fields we care about.
   def unapply(arg: QueryJson): Option[(UUID, UUID, UUID, Long, Node, Node, List[QueryResultJson])] =
     Some((arg.queryId, arg.topicId, arg.userId, arg.startTime, arg.i2b2QueryText, arg.extraXml, arg.queryResults))
 }
@@ -39,11 +50,6 @@ trait QueryJsonTrait {
   val queryResults : List[QueryResultJson] = json.queryResults.as[List[QueryResultJson]]
 }
 
-trait QueryResultJsonTrait {
-  val json: Json
-  val queryResultId: UUID = json.queryResultId.as[UUID]
-}
-
 final class QueryResultJson(val json: Json) extends QueryResultJsonTrait {
   override def equals(that: scala.Any): Boolean = that match {
     case that:QueryResultJson => QueryResultJson.unapply(this) == QueryResultJson.unapply(that)
@@ -54,11 +60,21 @@ final class QueryResultJson(val json: Json) extends QueryResultJsonTrait {
 }
 
 object QueryResultJson {
+  def apply(json:Json): Option[QueryResultJson] =
+    if (json.is[QueryResultJson]) Some(json.as[QueryResultJson]) else None
+
   def unapply(queryResultJson: QueryResultJson):Option[UUID] = {
     Some(queryResultJson.queryResultId)
   }
 }
 
+// Not done
+trait QueryResultJsonTrait {
+  val json: Json
+  val queryResultId: UUID = json.queryResultId.as[UUID]
+}
+
+// Todo: Move into package object. Where?
 object Extractors {
   implicit val nodeExtractor: Extractor[Node, Json] = Json.extractor[String].map(XML.loadString)
   implicit val uuidExtractor: Extractor[UUID, Json] = Json.extractor[String].map(UUID.fromString)
