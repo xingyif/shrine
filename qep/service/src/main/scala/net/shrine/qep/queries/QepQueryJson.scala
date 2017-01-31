3c03b0a60235e1d84afe0e4cee24e63395575a7a
Ty <Ty_Coghlan@hms.harvard.edu>
2017-01-31 16:14:58 -0500

    fixed json structures
    Former-commit-id: 4f9ab3ff58b8ed36e7a9b059bad466b048285c5d
    Former-commit-id: 94cbba386fc718583b71e560a89fa8cbb9974012

--- qep/service/src/main/scala/net/shrine/qep/queries/QepQueryJson.scala
+++ qep/service/src/main/scala/net/shrine/qep/queries/QepQueryJson.scala
@@ -7,6 +7,7 @@ import rapture.json.jsonBackends.jawn._
 import Extractors._
 
 import scala.xml.{Node, XML}
+import scala.util.hashing.Hashing.default
 
 /**
   * @by ty
@@ -15,20 +16,16 @@ import scala.xml.{Node, XML}
 
 final class QueryJson(val json: Json) extends QueryJsonTrait {
   override def equals(that: scala.Any): Boolean = that match {
-    case that:QueryJson =>
-      this.queryId == that.queryId &&
-      this.topicId == that.topicId &&
-      this.userId == that.userId &&
-      this.startTime == that.startTime &&
-      this.i2b2QueryText == that.i2b2QueryText &&
-      this.extraXml == that.extraXml &&
-      this.queryResults == that.queryResults
+    case that:QueryJson => QueryJson.unapply(this) == QueryJson.unapply(that)
     case _ => false
   }
 
-  override def hashCode(): QueryId =
-    Objects.hash(this.queryId, this.topicId, this.userId, this.startTime, this.i2b2QueryText, this.extraXml, this.queryResults)
+  override def hashCode(): QueryId = default.hash(QueryJson.unapply(this).get)
+}
 
+object QueryJson {
+  def unapply(arg: QueryJson): Option[(UUID, UUID, UUID, Long, Node, Node, List[QueryResultJson])] =
+    Some((arg.queryId, arg.topicId, arg.userId, arg.startTime, arg.i2b2QueryText, arg.extraXml, arg.queryResults))
 }
 
 trait QueryJsonTrait {
@@ -37,7 +34,7 @@ trait QueryJsonTrait {
   val topicId: UUID = json.topicId.as[UUID]
   val userId : UUID = json.userId.as[UUID]
   val startTime: Long     = json.startTime.as[Long]
-  val i2b2QueryText: Node = json.i2b2Querytext.as[Node]
+  val i2b2QueryText: Node = json.i2b2QueryText.as[Node]
   val extraXml     : Node = json.extraXml.as[Node]
   val queryResults : List[QueryResultJson] = json.queryResults.as[List[QueryResultJson]]
 }
@@ -49,17 +46,22 @@ trait QueryResultJsonTrait {
 
 final class QueryResultJson(val json: Json) extends QueryResultJsonTrait {
   override def equals(that: scala.Any): Boolean = that match {
-    case that:QueryResultJson =>
-      this.queryResultId == that.queryResultId
+    case that:QueryResultJson => QueryResultJson.unapply(this) == QueryResultJson.unapply(that)
     case _ => false
   }
 
-  override def hashCode(): QueryId = Objects.hash(this.queryResultId)
+  override def hashCode(): QueryId = Objects.hash(QueryResultJson.unapply(this).get)
+}
+
+object QueryResultJson {
+  def unapply(queryResultJson: QueryResultJson):Option[UUID] = {
+    Some(queryResultJson.queryResultId)
+  }
 }
 
 object Extractors {
   implicit val nodeExtractor: Extractor[Node, Json] = Json.extractor[String].map(XML.loadString)
   implicit val uuidExtractor: Extractor[UUID, Json] = Json.extractor[String].map(UUID.fromString)
-  implicit val qresExtractor: Extractor[QueryResultJsonTrait, Json] = Json.extractor.map(new QueryResultJson(_))
-  implicit val querExtractor: Extractor[QueryJsonTrait, Json] = Json.extractor.map(new QueryJson(_))
+  implicit val qresExtractor: Extractor[QueryResultJson, Json] = Json.extractor[Json].map(new QueryResultJson(_))
+  implicit val querExtractor: Extractor[QueryJson, Json] = Json.extractor[Json].map(new QueryJson(_))
 }
