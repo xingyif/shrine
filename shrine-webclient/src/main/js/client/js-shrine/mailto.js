f09f024b62c82a079534f5b476a1ee93f0e998d0
ben-carmen <benjamin_carmen@hms.harvard.edu>
2017-02-21 11:53:41 -0500

    fix of https issue in mailto had to strip out quotes from e-mail address
    Former-commit-id: 3aba73cae4cc7e6526a28e89883ac77e47f8c17c
    Former-commit-id: 8502388071ebcb58ae207d71733e68d373e26c8c

--- shrine-webclient/src/main/js/client/js-shrine/mailto.js
+++ shrine-webclient/src/main/js/client/js-shrine/mailto.js
@@ -36,7 +36,7 @@
     }
 
     function initMailTo(address) {
-        var sMailto = 'mailto:' + address;
+        var sMailto = 'mailto:' + address.split('\"')[1];
         var sSubject = 'subject=' + encodeURIComponent('Question from a SHRINE User');
         var body = encodeURIComponent('Please enter the suggested information and your question. Your data steward will reply to this email.' +
         '\n\n***Never send patient information, passwords, or other sensitive information by email****' +
