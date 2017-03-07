08a42085025db365dbede92e52091588ae3cd0e4
bendcarmen <ben.hms.harvard@gmail.com>
2017-03-07 15:04:02 -0500

    latest
    Former-commit-id: 12852005f0ffbc5915b4c85d54b1a03160bfc803
    Former-commit-id: f73bf9fecc6f079366fdeae74760f765a819719c

--- js-tools/templates/aurelia-jspm/src/app.spec.js
+++
@@ -1,8 +0,0 @@
-import {App, App2} from 'src/app.js';
-
-describe('A test of app.js ', () => {
-    it('should not work',  () => {
-        var app = new App();
-        expect(app.message).toBe('Hello Aurelia');
-    });
-});
