37ab1b7dad4f386041b56ffd4db41df359c2ff54
bendcarmen <ben.hms.harvard@gmail.com>
2017-04-13 11:29:44 -0400

    Fix of removing sessionStorage too soon
    Former-commit-id: fbb2c78e1a707a0fc1809f23893cc9bbdb2d00a2
    Former-commit-id: dc90db96db4efd5987330730b532803959bc91d6

--- shrine-webclient/src/main/js/client/js-shrine/dist/main.js
+++ shrine-webclient/src/main/js/client/js-shrine/dist/main.js
@@ -7,12 +7,6 @@ System.register([], function (_export, _context) {
         aurelia.start().then(function () {
             return aurelia.setRoot('shell');
         });
-
-        var shrine = {
-            auth: sessionStorage.getItem('shrine.auth')
-        };
-        sessionStorage.removeItem('shrine.auth');
-        aurelia.use.instance('shrine', shrine);
     }
 
     _export('configure', configure);
