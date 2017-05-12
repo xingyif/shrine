265a528bc5a3dcc6549f350a2880da64744ce3a9
bendcarmen <ben.hms.harvard@gmail.com>
2017-05-12 15:08:19 -0400

    Possible fix of scoping issue when SHRINE controller overrides i2b2 _queryRun method
    Former-commit-id: af27705278efaee68bd13bf262b5dd4f6171550a
    Former-commit-id: a8a36cd8178b6874fcb69fbcbfde36de9258512b

--- shrine-webclient/src/main/js/client/js-shrine/dist/common/i2b2.service.js
+++ shrine-webclient/src/main/js/client/js-shrine/dist/common/i2b2.service.js
@@ -22,41 +22,41 @@ System.register(['ramda', './container'], function (_export, _context) {
                 _classCallCheck(this, I2B2Service);
 
                 var ctx = Container.of(context);
-                var nullOrSomething = _.curry(function (d, f, c) {
+                var applyIfExists = _.curry(function (d, f, c) {
                     return c.hasNothing() ? d : f(c);
                 })(Container.of(null));
                 var map = _.curry(function (el, v) {
                     return v.map(_.prop(el));
                 });
                 var prop = _.curry(function (el, c) {
-                    return nullOrSomething(map(el), c);
+                    return applyIfExists(map(el), c);
                 });
                 var i2b2 = _.compose(prop('i2b2'), prop('window'), prop('parent'));
                 var crc = _.compose(prop('CRC'), i2b2);
                 var events = _.compose(prop('events'), i2b2);
 
                 this.onResize = function (f) {
-                    return nullOrSomething(function (c) {
+                    return applyIfExists(function (c) {
                         return c.value.changedZoomWindows.subscribe(f);
                     }, events(ctx));
                 };
                 this.onHistory = function (f) {
-                    return nullOrSomething(function (c) {
+                    return applyIfExists(function (c) {
                         return c.value.ctrlr.history.events.onDataUpdate.subscribe(f);
                     }, crc(ctx));
                 };
                 this.onQuery = function (f) {
-                    return nullOrSomething(function (c) {
+                    return applyIfExists(function (c) {
                         return c.value.afterQueryInit.subscribe(f);
                     }, events(ctx));
                 };
                 this.loadHistory = function () {
-                    return nullOrSomething(function (c) {
+                    return applyIfExists(function (c) {
                         return c.value.view.history.doRefreshAll();
                     }, crc(ctx));
                 };
                 this.loadQuery = function (id) {
-                    return nullOrSomething(function (c) {
+                    return applyIfExists(function (c) {
                         return c.value.ctrlr.QT.doQueryLoad(id);
                     }, crc(ctx));
                 };
