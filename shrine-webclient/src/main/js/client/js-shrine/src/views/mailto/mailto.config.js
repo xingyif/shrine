95e91e52638c91276508e26c524db5f1aa2911ed
bendcarmen <ben.hms.harvard@gmail.com>
2017-06-06 12:32:04 -0400

    Roll in the fix of 2047
    Former-commit-id: 4edd0bfddeb199018de322b9e79c3b2da0d41ba4
    Former-commit-id: 43267269573f239f2e91acef33e46866d6e3ca66

--- shrine-webclient/src/main/js/client/js-shrine/src/views/mailto/mailto.config.js
+++ shrine-webclient/src/main/js/client/js-shrine/src/views/mailto/mailto.config.js
@@ -1,12 +1,12 @@
 export const MailConfig = {
     mailto: 'mailto:',
-    subject: 'subject=' + encodeURIComponent('Question from a SHRINE User'),
-    body : encodeURIComponent('Please enter the suggested information and your question. Your data steward will reply to this email.' +
-        '\n\n***Never send patient information, passwords, or other sensitive information by email****' +
-        '\nName:' +
-        '\nTitle:' +
-        '\nUser name (to log into SHRINE):' +
-        '\nTelephone Number (optional):' +
-        '\nPreferred email address (optional):' +
-        '\n\nQuestion or Comment:')
+    subject: encodeURIComponent('Question from a SHRINE User'),
+    body : encodeURIComponent(`Please enter the suggested information and your question. Your data steward will reply to this email.
+        \n\n***Never send patient information, passwords, or other sensitive information by email****
+        \nName:
+        \nTitle:
+        \nUser name (to log into SHRINE):
+        \nTelephone Number (optional):
+        \nPreferred email address (optional):
+        \n\nQuestion or Comment:`)
 };
