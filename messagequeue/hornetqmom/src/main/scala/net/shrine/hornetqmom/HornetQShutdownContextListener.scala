c60b67f74f853ecbdbbff099a4eeae29add91669
yifan xing <xing.yif@husky.neu.edu>
2017-09-01 10:17:49 -0400

    cleaned up unnecessary comments
    Former-commit-id: cb3d0a42b4c40ac1d5d005753a64bcaf19231d30
    Former-commit-id: 254db541c6c87ead7c13e0af28899eadab8b6818

--- messagequeue/hornetqmom/src/main/scala/net/shrine/hornetqmom/HornetQShutdownContextListener.scala
+++ messagequeue/hornetqmom/src/main/scala/net/shrine/hornetqmom/HornetQShutdownContextListener.scala
@@ -9,20 +9,14 @@ import spray.routing.directives.LogEntry
   */
 
 class HornetQShutdownContextListener extends ServletContextListener {
-  private val hornetQServer: String = "HornetQServer"
-  private val hornetQServerStopper: String = "HornetQServerStopper"
 
 
   override def contextInitialized(servletContextEvent: ServletContextEvent): Unit = {
-//    val ctx = servletContextEvent.getServletContext
-//    ctx.setAttribute(hornetQServer, LocalHornetQMom)
-//    ctx.setAttribute(hornetQServerStopper, LocalHornetQMomStopper)
     LogEntry(s"Tomcat context initialized for meta-app", Logging.InfoLevel)
 
   }
 
   override def contextDestroyed(servletContextEvent: ServletContextEvent): Unit = {
-//    val ctx = servletContextEvent.getServletContext
     LocalHornetQMomStopper.stop()
     LogEntry(s"Tomcat context destroyed and HornetQ server is stopped for meta-app", Logging.InfoLevel)
   }
