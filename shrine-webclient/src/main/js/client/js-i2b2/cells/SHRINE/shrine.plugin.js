08212bf4db418d7ff5ae72e30f4a566d41766a28
bendcarmen <ben.hms.harvard@gmail.com>
2017-09-06 11:38:37 -0400

    Test of new error message
    Former-commit-id: da8e1880880144262f3f1d8e8df9ed9def3f39a7
    Former-commit-id: c387df05ed24a753e13fc47176d0c2fe6f99e1e5

--- shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/shrine.plugin.js
+++ shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/shrine.plugin.js
@@ -99,7 +99,7 @@
                 text: "Done",
                 handler: function() {this.cancel();},
                 isDefault: true
-            }]
+            }]11
         });
 
         //$ = prototype.js
@@ -117,10 +117,13 @@
 
     // ES5 :(
     function getDialogHTML(data) {
-        return '<div id="pluginErrorDetail" style="display:none;">' +
+        return 
+            '<div id="pluginErrorDetail" style="display:none;">' +
             '<div class="hd" style="background:#6677AA;">SHRINE Result Status</div>' +
             '<div class="bd">' +
             '<br />' +
+            '<button>More Detail </button>' +
+            '<button style="display: none">Less Detail</button>' +
             '<div style="border: 1px solid #C0C0C0; max-height: 450px;' +
             'background-color: #FFFFFF; overflow: scroll; word-wrap: break-word; padding: 10px 5px;"' +
             'id="pluginErrorDetail" class="StatusBoxText">' +
@@ -128,8 +131,43 @@
             '<div>' + data.status + '</div><br/>' +
             '<div><b>Description:</b></div>' +
             '<div><p>' + data.statusMessage + '</p></div><br/>' +
+            '<span id="pluginMoreDetail" style="display:none">' + 
+                '<div><b>Codec:</b></div>' +
+                '<div>' + data.problemDigest && data.problemDigest.codec? data.problemDigest.codec : 'not available' + '</div><br/>' +
+                '<div><b>Stamp:</b></div>' +
+                '<div>' + data.problemDigest && data.problemDigest.stampText? data.problemDigest.stampText : 'not available' + '</div><br/>' +
+                '<div><b>Stack Trace Name:</b></div>' +
+                '<div>' + data.problemDigest && data.problemDigest.codec? data.problemDigest.codec : 'not available' + '</div><br/>' +
+                '<div><b>Stack Trace Message:</b></div>' +
+                '<div>' + data.problemDigest && data.problemDigest.description? data.problemDigest.description : 'not available' + '</div><br/>' +
+                '<div><b>Stack Trace Details:</b></div>' +
+                '<div>' + data.problemDigest && data.problemDigest.detailsString? data.problemDigest.detailsString.split(',').join(',\n') : 'not available' + '</div><br/>' +
+            '</span>'
             '</div>' +
             '</div>' +
             '</div>';
     }
 })();
+
+<div id="dialogErrorDetail" style="display:none;">
+    <div class="hd" style="background:#6677AA;">Query Error Detail</div>
+    <div class="bd">
+        <br />
+        <button id='btnExpandErrorDetail'>More
+            Detail
+        </button>
+        <button id='btnContractErrorDetail'
+                style='display: none'>Less Detail
+        </button>
+        <a id="dlgNetworkStatus"
+           style="cursor: pointer; position:absolute; right:20px;
+               top: 58px; text-decoration:
+               underline; color:blue; display: none">Network
+            Status</a>
+        <div style="border: 1px solid #C0C0C0; max-height: 450px;
+        background-color: #FFFFFF; overflow: scroll; word-wrap: break-word; padding: 10px 5px;"
+             id="errorDetailDiv" class="StatusBoxText">
+        </div>
+    </div>
+</div>
+
