4bfe7483784e3f084c32eb9d86a58c330812139f
bendcarmen <ben.hms.harvard@gmail.com>
2017-10-03 13:32:36 -0400

    Possible fix of undefined in query export
    Former-commit-id: b78f921f6f6f05588271df8f1e92c4e93d57424c
    Former-commit-id: 8c0f2a36602c64ac7e843e27465e8b6452634698

--- shrine-webclient/src/main/js/client/js-shrine/src/services/query-export.js
+++ shrine-webclient/src/main/js/client/js-shrine/src/services/query-export.js
@@ -1,7 +1,7 @@
-import {PubSub} from './pub-sub';
-export class QueryExport extends PubSub{
+import { PubSub } from './pub-sub';
+export class QueryExport extends PubSub {
     constructor(...rest) {
-        super(...rest); 
+        super(...rest);
     }
     listen() {
         this.subscribe(this.commands.shrine.exportResult, convertObjectToCSV);
@@ -10,25 +10,26 @@ export class QueryExport extends PubSub{
 
 const convertObjectToCSV = (d) => {
     const nodes = d.nodes.sort();
+    const desc = ({ resultType: { i2b2Options: { description } } }) => description;
+    const brdSort = (a, b) => desc(a) <= desc(b) ? -1 : 1;
     const m = new Map();
-    const desc = ({resultType:{i2b2Options:{description}}}) => description;
-    const brdSort = (a,b) => desc(a) <= desc(b)? -1 : 1;
-    nodes.forEach(({breakdowns = []}) => {
-        breakdowns.sort(brdSort).forEach(({resultType:{i2b2Options:{description}}, results}) => 
-            m.has(description)? m.get(description).add(...results.map(r => r.dataKey).sort()) : m.set(description, new Set(results.map(r => r.dataKey).sort()))
-        )
+    nodes.forEach(({ breakdowns = [] }) => {
+        breakdowns.sort(brdSort).forEach(({ resultType: { i2b2Options: { description = null} }, results = []}) => {
+            if(!description === null || results.length === 0) return;
+            m.has(description) ? m.get(description).add(...results.map(r => r.dataKey).sort()) : m.set(description, new Set(results.map(r => r.dataKey).sort()))
+        });
     });
 
     const line1 = `SHRINE QUERY RESULTS (OBFUSCATED PATIENT COUNTS),${['', ...nodes.map(n => n.adapterNode).join(',')]}`;
-    const line2 = `\nAll Patients,${['', ...nodes.map(n => n.count? (n.count > 0? n.count : 0) : 'unavailable').join(',')]}`;
+    const line2 = `\nAll Patients,${['', ...nodes.map(n => n.count ? (n.count > 0 ? n.count : 0) : 'unavailable').join(',')]}`;
     const result = [];
     m.forEach((v, k) => {
-        result.push('',...Array.from(v).map(s => {
+        result.push('', ...Array.from(v).map(s => {
             const title = `${k.split(' ').shift()},${s}`;
-            const values = nodes.map(({breakdowns = []}) => {
-                const b = breakdowns.find(({resultType:{i2b2Options:{description}}, results}) => description === k);
-                const r = b? b.results.find(r => r.dataKey === s) : undefined;
-                return !r? 'unavailable': r.value > 0? r.value : 0;
+            const values = nodes.map(({ breakdowns = [] }) => {
+                const b = breakdowns.find(({ resultType: { i2b2Options: { description } }, results }) => description === k);
+                const r = b ? b.results.find(r => r.dataKey === s) : undefined;
+                return !r ? 'unavailable' : r.value > 0 ? r.value : 0;
             });
             return `${title},${values.join(",")}`;
         }));
@@ -38,14 +39,14 @@ const convertObjectToCSV = (d) => {
 }
 
 const exportIEWebkitGecko = (csv, context = window.parent) => {
-    if(context === undefined) return;
+    if (context === undefined) return;
     const filename = "export.csv";
     const blob = new Blob([csv]);
     const isIE = context.navigator !== undefined && context.navigator.msSaveOrOpenBlob !== undefined
     if (isIE) context.navigator.msSaveBlob(blob, filename);
     else {
         const a = context.parent.document.createElement('a');
-        a.href = context.URL.createObjectURL(blob, {type: "text/plain"});
+        a.href = context.URL.createObjectURL(blob, { type: "text/plain" });
         a.download = filename;
         context.document.body.appendChild(a);
         a.click();  // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
