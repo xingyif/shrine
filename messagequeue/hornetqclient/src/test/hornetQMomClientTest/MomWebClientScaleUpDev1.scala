cb92ba5536126316386bed9195206f812114dfa0
yifan xing <xing.yif@husky.neu.edu>
2017-10-18 11:07:08 -0400

    scale up test to 30 sends, 90 receives, 15seconds receive time out
    Former-commit-id: 35465173c3df9974432f55db30d4df0f161fc20c
    Former-commit-id: 35c64086700d7856e2fb37e36cb02e8fd0098425

--- messagequeue/hornetqclient/src/test/hornetQMomClientTest/MomWebClientScaleUpDev1.scala
+++ messagequeue/hornetqclient/src/test/hornetQMomClientTest/MomWebClientScaleUpDev1.scala
@@ -14,47 +14,54 @@
 // #2. Load file in REPL:
 // :load <path-to-file>
 
-import java.util
-
 import net.shrine.hornetqclient.HornetQMomWebClient
 import net.shrine.messagequeueservice.{Message, Queue}
 import net.shrine.source.ConfigSource
 
-import scala.concurrent.duration.Duration
 import scala.collection.immutable.Seq
 import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration.Duration
 
   val configMap: Map[String, String] = Map( "shrine.messagequeue.blockingq.serverUrl" -> "https://shrine-dev1.catalyst:6443/shrine-metadata/mom")
 
   ConfigSource.atomicConfig.configForBlock(configMap, "HornetQMomClientDev1") {
-    val scale: Int = 10
-    val multiplier: Int = 3
+    val scale: Int = 50
+    val multiplier: Int = 2
     println(s"Running tests on ${HornetQMomWebClient.momUrl}")
 
     val queueNameDev1: String = "dev1Queue"
-    val dev2Queue: Queue = HornetQMomWebClient.createQueueIfAbsent(queueNameDev1).get
+    val dev1Queue: Queue = HornetQMomWebClient.createQueueIfAbsent(queueNameDev1).get
+    val queueNameDev2: String = "dev2Queue"
+    val dev2Queue: Queue = HornetQMomWebClient.createQueueIfAbsent(queueNameDev2).get
+
     val allQueues: Seq[Queue] = HornetQMomWebClient.queues.get
     println(s"All Existing Queues: $allQueues")
 
     for (i <- 1 to scale) {
-      val sendTry = HornetQMomWebClient.send(s"Message$i sent to $queueNameDev1", dev2Queue)
+      val sendTry = HornetQMomWebClient.send(s"Message$i sent to $queueNameDev1", dev1Queue)
       println(s"Sending messages to dev1, attempt: $i, $sendTry")
     }
 
-    val firstDuration: Duration = Duration.create(1, "seconds")
+    val firstDuration: Duration = Duration.create(15, "seconds")
     val listOfMessages: ArrayBuffer[Option[Message]] = ArrayBuffer()
     for (i <- 1 to scale * multiplier) {
       val receivedOpt: Option[Message] = HornetQMomWebClient.receive(dev2Queue, firstDuration).get
       listOfMessages += receivedOpt
-      println(s"Receiving messages to dev1, attempt: $i, $receivedOpt")
+      println(s"Receiving messages from dev2, attempt: $i, $receivedOpt")
     }
 
-    listOfMessages.foreach(msgOpt => msgOpt.map(m => m.complete()))
-//    for (i <-1 to 10) {
-//      val messageOpt: Option[Message] = listOfMessages(i)
-//      messageOpt.map(m => m.complete())
-//    }
+    listOfMessages.foreach(msgOpt => {
+      msgOpt.map(m => {
+        m.complete()
+        println(s"Completed Message $m")
+      })
+    })
 
-    val deleteTry = HornetQMomWebClient.deleteQueue(queueNameDev1)
-    println(s"Deleted queue: $queueNameDev1, $deleteTry")
+    for (i <- 1 to scale) {
+      val receivedOpt: Option[Message] = HornetQMomWebClient.receive(dev1Queue, firstDuration).get
+      listOfMessages += receivedOpt
+      println(s"Should have no Message (all completed), Receiving messages from dev1, attempt: $i, $receivedOpt")
+    }
+//    val deleteTry = HornetQMomWebClient.deleteQueue(queueNameDev1)
+//    println(s"Deleted queue: $queueNameDev1, $deleteTry")
   }
--- messagequeue/hornetqclient/src/test/hornetQMomClientTest/MomWebClientScaleUpDev2.scala
+++ messagequeue/hornetqclient/src/test/hornetQMomClientTest/MomWebClientScaleUpDev2.scala
@@ -25,34 +25,45 @@ import scala.concurrent.duration.Duration
 val configMap: Map[String, String] = Map( "shrine.messagequeue.blockingq.serverUrl" -> "https://shrine-dev2.catalyst:6443/shrine-metadata/mom")
 
 ConfigSource.atomicConfig.configForBlock(configMap, "HornetQMomClientDev2") {
-  val scale: Int = 10
+  val scale: Int = 50
+  val multiplier: Int = 2
   println(s"Running tests on ${HornetQMomWebClient.momUrl}")
 
 
+  val queueNameDev1: String = "dev1Queue"
+  val dev1Queue: Queue = HornetQMomWebClient.createQueueIfAbsent(queueNameDev1).get
   val queueNameDev2: String = "dev2Queue"
   val dev2Queue: Queue = HornetQMomWebClient.createQueueIfAbsent(queueNameDev2).get
+
   val allQueues: Seq[Queue] = HornetQMomWebClient.queues.get
   println(s"All Existing Queues: $allQueues")
 
   for (i <- 1 to scale) {
     val sendTry = HornetQMomWebClient.send(s"Message$i sent to $queueNameDev2", dev2Queue)
-    println(s"Sending messages to dev1, attempt: $i, $sendTry")
+    println(s"Sending messages to dev2, attempt: $i, $sendTry")
   }
 
-  val firstDuration: Duration = Duration.create(1, "seconds")
+  val firstDuration: Duration = Duration.create(15, "seconds")
   val listOfMessages: ArrayBuffer[Option[Message]] = ArrayBuffer()
+  for (i <- 1 to scale*multiplier) {
+    val receivedOpt: Option[Message] = HornetQMomWebClient.receive(dev1Queue, firstDuration).get
+    listOfMessages += receivedOpt
+    println(s"Receiving messages from dev1, attempt: $i, $receivedOpt")
+  }
+
+  listOfMessages.foreach(msgOpt => msgOpt.map({
+    m => {
+      m.complete()
+      println(s"Completed Message $m")
+    }
+  }))
+
   for (i <- 1 to scale) {
     val receivedOpt: Option[Message] = HornetQMomWebClient.receive(dev2Queue, firstDuration).get
     listOfMessages += receivedOpt
-    println(s"Receiving messages to dev1, attempt: $i, $receivedOpt")
+    println(s"Should have no Message (all completed), Receiving messages from dev2, attempt: $i, $receivedOpt")
   }
 
-  listOfMessages.foreach(msgOpt => msgOpt.map(m => m.complete()))
-  //    for (i <-1 to 10) {
-  //      val messageOpt: Option[Message] = listOfMessages(i)
-  //      messageOpt.map(m => m.complete())
-  //    }
-
-  val deleteTry = HornetQMomWebClient.deleteQueue(queueNameDev2)
-  println(s"Deleted queue: $queueNameDev2, $deleteTry")
+//  val deleteTry = HornetQMomWebClient.deleteQueue(queueNameDev2)
+//  println(s"Deleted queue: $queueNameDev2, $deleteTry")
 }
