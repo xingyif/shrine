0be3c034784552681fa1f5d88b3ac021389522f7
bendcarmen <ben.hms.harvard@gmail.com>
2017-10-19 16:29:11 -0400

    Fix of display query bug
    Former-commit-id: 9e09a4253420b47a707c53b7049ae9585710e7ff
    Former-commit-id: 9b4196bafd2357c2e348d72047b7f1c296fcb254

--- shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/common/shrine.base.js
+++ shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/common/shrine.base.js
@@ -6,6 +6,41 @@ export default class ShrineBase {
     this.i2b2.events.afterCellInit.subscribe(function(en,co) {
       console.log('after cell init!');
     });
+
+    this.i2b2.events.afterAllCellsLoaded.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.afterFrameworkInit.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.afterHiveInit.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.afterLogin.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.changedViewMode.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.changedZoomWindows.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events.initView.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+
+    this.i2b2.events._privRemoveInitFuncs.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
+    this.i2b2.events._privLoadCells.subscribe(function(en,co) {
+      console.log('after cell init!');
+    });
   }
 
   get global() {
--- shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/mixins/CRC.view.History.js
+++ shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/mixins/CRC.view.History.js
@@ -1,10 +1,11 @@
 export const contextMenuValidateMixin = context => {
   const ContextMenuValidate = context.i2b2.CRC.view.history.ContextMenuValidate;
-  const ITEM_TEXT = 'Display'; 
+  const ITEM_TEXT = 'Display';
   const needsDisplayQuery = items => items.filter(i => i.text === ITEM_TEXT).length === 0;
-  return event => {
+
+  return function (p_oEvent) {
     const itemData = context.i2b2.CRC.view.history.ContextMenu.itemData;
-    if(needsDisplayQuery(context.i2b2.CRC.view.history.ContextMenu.itemData)){
+    if(needsDisplayQuery(itemData)){
       itemData.unshift({ 
         text: ITEM_TEXT, 
         onClick: {
@@ -17,7 +18,32 @@ export const contextMenuValidateMixin = context => {
           }
         }
       })
-      ContextMenuValidate(event);
+    }
+
+
+    var clickId = null;
+    var currentNode = this.contextEventTarget;
+    while (!currentNode.id) {
+      if (currentNode.parentNode) {
+        currentNode = currentNode.parentNode;
+      } else {
+        // we have recursed up the tree to the window/document DOM... it's a bad click
+        this.cancel();
+        return;
+      }
+    }
+    clickId = currentNode.id;
+    // see if the ID maps back to a treenode with SDX data
+    var tvNode = context.i2b2.CRC.view.history.yuiTree.getNodeByProperty('nodeid', clickId);
+    if (tvNode) {
+      if (tvNode.data.i2b2_SDX) {
+        if (tvNode.data.i2b2_SDX.sdxInfo.sdxType == "QM") {
+          context.i2b2.CRC.view.history.contextRecord = tvNode.data.i2b2_SDX;
+        } else {
+          this.cancel();
+          return;
+        }
+      }
     }
   };
 }
