ddcf1db4b9cde4c99d551e96530503e549502e22
bendcarmen <ben.hms.harvard@gmail.com>
2017-10-19 08:53:31 -0400

    Fix of topics loading issue
    Former-commit-id: 9952e8fa449946417d6dfdf786ef01d3025de95b
    Former-commit-id: 94caa905dc829667f3e21c5cdc838edb023183af

--- shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/shrine.messenger.js
+++ shrine-webclient/src/main/js/client/js-i2b2/cells/SHRINE/src/shrine.messenger.js
@@ -9,7 +9,8 @@ class ShrineMessenger extends I2B2Decorator {
 	decorate() {
 		this.shrine.ajax = this.i2b2.hive.communicatorFactory("SHRINE");
 		this.shrine.cfg.msgs = {readApprovedEntries: APPROVED_ENTRY_XML};
-		this.shrine.cfg.parsers = {readApprovedEntries}
+		let readApprovedEntries = this.readApprovedEntries(this);
+		this.shrine.cfg.parsers = {readApprovedEntries};
 		this.shrine.ajax._addFunctionCall(
 			"readApprovedEntries",
 			this.shrine.cfg.config.readApprovedURL,
@@ -18,26 +19,27 @@ class ShrineMessenger extends I2B2Decorator {
 			this.shrine.cfg.parsers.readApprovedEntries
 		);
 	}
-}
-
-// -- method will be altered by i2b2 i.e. this.model, this.error will be added properties ---//
-function readApprovedEntries() {
-	if (!this.error) {
-		this.model = [];
-		var qm = this.refXML.getElementsByTagName('sheriffEntry');
-		for (var i = 0; i < 1 * qm.length; i++) {
-			var o = new Object;
-			o.xmlOrig = qm[i];
-			o.approval = this.i2b2.h.getXNodeVal(qm[i], 'approval');
-			o.TopicID = this.i2b2.h.getXNodeVal(qm[i], 'queryTopicID');
-			o.Name = this.i2b2.h.getXNodeVal(qm[i], 'queryName');
-			this.model.push(o);
+	//return 'function instead of =>' because i2b2 treats this as both an ES5 scope and a method.
+	readApprovedEntries(context) {
+		return function() {
+			if (!this.error) {
+				this.model = [];
+				var qm = this.refXML.getElementsByTagName('sheriffEntry');
+				for (var i = 0; i < 1 * qm.length; i++) {
+					var o = new Object;
+					o.xmlOrig = qm[i];
+					o.approval = context.i2b2.h.getXNodeVal(qm[i], 'approval');
+					o.TopicID = context.i2b2.h.getXNodeVal(qm[i], 'queryTopicID');
+					o.Name = context.i2b2.h.getXNodeVal(qm[i], 'queryName');
+					this.model.push(o);
+				}
+			} else {
+				this.model = false;
+				console.error("[readApprovedEntries] Could not parse() data!");
+			}
+			return this;
 		}
-	} else {
-		this.model = false;
-		console.error("[readApprovedEntries] Could not parse() data!");
 	}
-	return this;
 }
 
 // -- singleton --//
