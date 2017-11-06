eda197035c03b96c70435437086015f109407bc2
bendcarmen <ben.hms.harvard@gmail.com>
2017-11-06 08:26:28 -0500

    Testing a different order of history refresh
    Former-commit-id: bad450cbf58c32cfc6543e5d4bdfe80e3acb9e89
    Former-commit-id: b29ca9b6ddf68b4a8b185ca6745767896adc4140

--- shrine-webclient/src/main/js/shrine-plugin/dist/views/query-status/query-status.js
+++ shrine-webclient/src/main/js/shrine-plugin/dist/views/query-status/query-status.js
@@ -169,9 +169,7 @@ System.register(['aurelia-framework', 'services/query-status.model', 'services/p
                         _this2.nodes = nodes;
                         if (!complete) {
                             _this2.publish(_this2.commands.shrine.fetchQuery, { networkId: networkId, dataVersion: dataVersion, timeoutSeconds: timeoutSeconds });
-                        } else {
-                            _this2.publish(_this2.notifications.shrine.refreshAllHistory);
-                        }
+                        } else {}
                     });
 
                     if (me.get(this).isDevEnv) {
