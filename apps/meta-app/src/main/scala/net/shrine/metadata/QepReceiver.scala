45159fad9da65fea8a9e6a1fbd46b1225b78f7da
yifan xing <xing.yif@husky.neu.edu>
2017-11-14 12:30:38 -0500

    optimize imports
    Former-commit-id: 0710a2821ef9ed3e2ec832a4da32c7bb3cec8630
    Former-commit-id: 2443414b646b411e5dab7528bb728f2269edb144

--- apps/meta-app/src/main/scala/net/shrine/metadata/QepReceiver.scala
+++ apps/meta-app/src/main/scala/net/shrine/metadata/QepReceiver.scala
@@ -8,13 +8,12 @@ import net.shrine.broadcaster.{IdAndUrl, NodeListParser}
 import net.shrine.config.ConfigExtensions
 import net.shrine.log.Log
 import net.shrine.messagequeueservice.protocol.Envelope
-import net.shrine.messagequeueservice.{CouldNotCompleteMomTaskButOKToRetryException, CouldNotCompleteMomTaskDoNotRetryException, Message, MessageQueueService, Queue}
+import net.shrine.messagequeueservice.{CouldNotCompleteMomTaskButOKToRetryException, Message, MessageQueueService, Queue}
 import net.shrine.problem.{AbstractProblem, ProblemSources}
 import net.shrine.protocol.{AggregatedRunQueryResponse, QueryResult, ResultOutputType, ResultOutputTypes}
 import net.shrine.qep.querydb.{QepQueryDb, QepQueryDbChangeNotifier, QueryResultRow}
 import net.shrine.source.ConfigSource
 import net.shrine.status.protocol.IncrementalQueryResult
-import spray.http.StatusCodes
 
 import scala.concurrent.duration.Duration
 import scala.util.control.NonFatal
