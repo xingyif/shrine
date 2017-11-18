56cc6e7ad558c42e83ad6fb603ea697af92a7c24
bendcarmen <ben.hms.harvard@gmail.com>
2017-11-18 11:07:37 -0600

    reset
    Former-commit-id: 4eb724442975a6ecf58be5f95116550a37c741e6
    Former-commit-id: 12177aeb09c351c0703fb0ce8df6b012658fa8e7

--- shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
+++ shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
@@ -2,7 +2,7 @@ const getChildRecordsMixin = context => (sdxParentNode, onCompleteCallback) => {
   const {origData:{id:networkId}} = sdxParentNode;
   context.i2b2.CRC.ctrlr.QT.doQueryLoad(networkId);
   context.i2b2.CRC.view.history.yuiTree.locked = false;
-  context.i2b2.CRC.view.history.doRefreshAll(); //TODO:  trying this out.
+  //context.i2b2.CRC.view.history.doRefreshAll(); //TODO:  trying this out.
   //in CRC_view_History.js:
   //see i2b2.CRC.view.history.PopulateQueryMasters = function(dm_ptr, dm_name, options) { 
   //TODO:  what about re-loading history?
