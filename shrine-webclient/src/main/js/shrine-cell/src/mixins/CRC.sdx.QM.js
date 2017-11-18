66328d181eabe05be03587ee0a82c210f53643d5
bendcarmen <ben.hms.harvard@gmail.com>
2017-11-18 10:34:08 -0600

    Test of locking previous queries fix
    Former-commit-id: 15154297db34d5d6cb86498d2b16434ca6bd011f
    Former-commit-id: b2f82b0f8cb0f62dcc8097bc32687a79ea4df71d

--- shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
+++ shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
@@ -2,6 +2,16 @@ const getChildRecordsMixin = context => (sdxParentNode, onCompleteCallback) => {
   const {origData:{id:networkId}} = sdxParentNode;
   context.i2b2.CRC.ctrlr.QT.doQueryLoad(networkId);
   context.i2b2.CRC.view.history.yuiTree.locked = false;
+  context.i2b2.CRC.view.history.doRefreshAll(); //TODO:  trying this out.
+  //in CRC_view_History.js:
+  //see i2b2.CRC.view.history.PopulateQueryMasters = function(dm_ptr, dm_name, options) { 
+  //TODO:  what about re-loading history?
+  /*context.i2b2.CRC.view.history.yuiTree.nodes.map(n => {
+    n.locked = false;
+  });*/
+
+  //OR
+  //context.i2b2.CRC..view.history.doRefreshAll());
 }
 
 export default getChildRecordsMixin;
