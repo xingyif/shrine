966ee27fb322d3430c5ead46e1e42cc07686289b
bendcarmen <ben.hms.harvard@gmail.com>
2017-11-20 08:51:25 -0600

    Updated the version to 1.24.1.3-SNAPSHOT
    Former-commit-id: 5e47cb989a2c7cb5255a8703453a961df0688326
    Former-commit-id: 502104d38b6932133aa549192fca7e1346326101

--- shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
+++ shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.sdx.QM.js
@@ -1,17 +1,10 @@
 const getChildRecordsMixin = context => (sdxParentNode, onCompleteCallback) => {
-  const {origData:{id:networkId}} = sdxParentNode;
-  context.i2b2.CRC.ctrlr.QT.doQueryLoad(networkId);
+  const {origData:{id:networkId,name}} = sdxParentNode;
+  //TODO: fix destructuring logic above!!!
+  context.i2b2.events.networkIdReceived.fire({networkId, name: cr.origData.name});
+  context.i2b2.CRC.ctrlr.QT.doQueryLoad(cr.origData.id);
   context.i2b2.CRC.view.history.yuiTree.locked = false;
-  //context.i2b2.CRC.view.history.doRefreshAll(); //TODO:  trying this out.
-  //in CRC_view_History.js:
-  //see i2b2.CRC.view.history.PopulateQueryMasters = function(dm_ptr, dm_name, options) { 
-  //TODO:  what about re-loading history?
-  /*context.i2b2.CRC.view.history.yuiTree.nodes.map(n => {
-    n.locked = false;
-  });*/
-
-  //OR
-  //context.i2b2.CRC..view.history.doRefreshAll());
+  context.i2b2.CRC.view.history.yuiTree._nodes.map(n => n.isLoading = false);
 }
 
 export default getChildRecordsMixin;
