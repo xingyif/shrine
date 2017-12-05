b1fe19ca055a1d1e705ed2a76f161bd3a14e6107
bendcarmen <ben.hms.harvard@gmail.com>
2017-12-05 10:23:27 -0500

    Remove highlight if new query is run
    Former-commit-id: 0dc2f374aa12f0629cc0832ac4b4a2938e606169
    Former-commit-id: 4f920e2db0ac4bb6c58b1365cfffecf2550a9c70

--- shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.ctrlr.QryTool.js
+++ shrine-webclient/src/main/js/shrine-cell/src/mixins/CRC.ctrlr.QryTool.js
@@ -2,6 +2,7 @@ export const queryRunMixin = (context) =>
   (inQueryName, options) => {
 
     context.shrine.plugin.disableRunQueryButton();
+    context.$('.sdxDefaultQM').css('background-color', 'white');
     context.i2b2.events.afterQueryInit.fire({ name: inQueryName, data: options });
     
     // make sure name is not blank
